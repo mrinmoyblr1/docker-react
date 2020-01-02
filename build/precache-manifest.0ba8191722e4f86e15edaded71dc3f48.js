c185a06a3b3ace41c044dcb05a1d69cbbfef1cc2
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2020-01-02 20:09:57 +0530

    First Commit New

---
+++ build/precache-manifest.0ba8191722e4f86e15edaded71dc3f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860829155fb12cc940455a82f507bbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7041ff9cedeac7d52233",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "659c61345ac09ddc5acb",
+    "url": "/static/js/2.d9115e9f.chunk.js"
+  },
+  {
+    "revision": "7041ff9cedeac7d52233",
+    "url": "/static/js/main.3106d385.chunk.js"
+  },
+  {
+    "revision": "4ddea4c644bf9703e3c0",
+    "url": "/static/js/runtime-main.5beb5e24.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
